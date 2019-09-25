b2e3c2790b726bf99c305a5baade7e1553f1966d
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-25 19:28:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
-  reversed_array = sentence_array.each{|b| reversed_array.push(b.reverse)} 
+  reversed_array = []
+  reversed_array << sentence_array.each{|b| b.reverse} 
   p reversed_array.join(" ")
 end
